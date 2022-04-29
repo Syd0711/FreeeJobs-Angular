64f0618b47ae9ac0f52129794496b7b5e3898943
unknown <skyxsw7@gmail.com>
2022-04-29 17:45:22 +0800

    change proxy settings

--- server.js
+++ server.js
@@ -8,25 +8,25 @@ res.sendFile(path.join(__dirname+
 '/dist/freeejob/index.html'));});
 
 app.use('/jobListing/*', createProxyMiddleware({
-    target: "https://freeejobs-joblisting-ms.herokuapp.com/",
+    target: "https://freeejobs-joblisting-ms.herokuapp.com",
     secure: false,
     logLevel: "debug",
     changeOrigin: true
   }));
 app.use('/jobApplication/*', createProxyMiddleware({
-    target: "https://freeejobs-application-ms.herokuapp.com/",
+    target: "https://freeejobs-application-ms.herokuapp.com",
     secure: false,
     logLevel: "debug",
     changeOrigin: true
   }));
 app.use('/iam/*', createProxyMiddleware({
-    target: "https://freeejobs-iam-ms.herokuapp.com/",
+    target: "https://freeejobs-iam-ms.herokuapp.com",
     secure: false,
     logLevel: "debug",
     changeOrigin: true
   }));
 app.use('/rating/*', createProxyMiddleware({
-    target: "https://freeejobs-rating-ms.herokuapp.com/",
+    target: "https://freeejobs-rating-ms.herokuapp.com",
     secure: false,
     logLevel: "debug",
     changeOrigin: true
